79cc45220cd5ba7b1b78d217c8a3d2138b9f8726
peterpih <ppih@panix.com>
2016-05-13 13:32:21 -0500

    Timezone, Export To: address
    add back .to_s for address
    
    refactor export
    
    Attach files, Users export
    
    Add Privacy Policy to tick box on Membership page
    
    upgrade Ruby->2.3.1
    
    Additional links to User Profile for editting
    
    Add sitemap
    
    Add current page to Page Title for unique tabs
    
    Add 301 redirects for some 404's in Google Search Console
    
    more redirects
    
    additional redirects
    
    show ID and Content-Type in Admin for Attach Files
    
    Fix for Attached Files
    
    Added .form_section_header to some forms
    
    additional verbage on New Member page from Bob
    
    Attached_files: improve content_type determination, add updated_at in index
    
    Additional links to User Profile for editting
    
    Add current page to Page Title for unique tabs
    
    Change Signs of Life Newsletter to Magazine

--- app/models/attached_file.rb
+++ app/models/attached_file.rb
@@ -2,12 +2,16 @@ class AttachedFile < ActiveRecord::Base
   belongs_to :attachable, polymorphic: true
   
   paperclip_opts = { styles: { thumb: '200x200>' } }
-  if Rails.env.production?
+
+  # April 10, 2016 
+  # This block setup to use S3 in development and production environments
+  #
+  if Rails.env.production? || Rails.env.development?
     paperclip_opts.merge!(
       {
         storage: :s3,
         s3_credentials: {
-          bucket: 'fff_attached_files',
+          bucket: ENV['AWS_S3_BUCKET_NAME'],
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
         }
@@ -16,6 +20,36 @@ class AttachedFile < ActiveRecord::Base
   end
 
   has_attached_file :attachment, paperclip_opts
+
+  before_save :set_content_type
+
+  #
+  # explicitly set some content_type using file extension
+  # if not one of the explicits, then default to what paperclip thinks it is
+  #
+  def set_content_type
+    extension = File.extname(attachment_file_name).downcase
+    case extension
+      when '.pdf'
+        self.attachment_content_type = 'application/pdf'
+      when '.txt', ''
+        self.attachment_content_type = 'text/plain'
+      when '.csv.'
+        self.attachment_content_type = 'text/csv'
+      when '.jpg'
+        self.attachment_content_type = 'image/jpeg'
+      when '.png'
+        self.attachment_content_type = 'image/png'
+      when '.xml'
+        self.attachment_content_type = 'text/xml'
+      when '.bmp'
+        self.attachment_content_type = 'image/bmp'
+    end
+    self.attachment.options.merge!(s3_headers: {
+                                    'Content-Type' => self.attachment_content_type || 'text/plain'
+                                    })
+    self.attachment.send :initialize_storage
+  end
 
   do_not_validate_attachment_file_type :attachment
 
@@ -31,3 +65,4 @@ class AttachedFile < ActiveRecord::Base
   end
 
 end
+
